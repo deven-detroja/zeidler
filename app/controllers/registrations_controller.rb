edbf9b22e3e0e6e7d38b6a1e3fe1233ccf33f3f1
deven-detroja <deven.detroja@gmail.com>
2020-02-02 17:17:20 +0530

    Committing the changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,17 +4,11 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit( :first_name, :last_name, :age, :gender, :country, 
-                                  :email, 
-                                  :password, 
-                                  :password_confirmation)
+                                :email, :password, :password_confirmation)
   end
 
   def account_update_params
     params.require(:user).permit( :first_name, :last_name, :age, :gender, :country, 
-                                  :email, 
-                                  :password, 
-                                  :password_confirmation, 
-                                  :current_password)
+                                :email, :password, :password_confirmation, :current_password)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 
-    validates_presence_of :first_name, :message => 'Please Enter First Name'
-  	validates_presence_of :last_name, :message => 'Please Enter Last Name'
-  	validates_presence_of :country, :message => 'Please Enter Country Details'
+    PASSWORD_FORMAT = /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x
+    EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    ALPHABET_FORMAT = /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
 
+    validates :email, format: { with: EMAIL_FORMAT, message: 'Please enter valid Email formate' }
+    validates :password, format: {with: PASSWORD_FORMAT, message: 'Should contain at least 1 of each (upper case, lower case, number, special char)'}
+    validates_presence_of :first_name, message: 'Please Enter First Name'
+  	validates_presence_of :last_name, message: 'Please Enter Last Name'
+  	validates_presence_of :country, message: 'Please Enter Country Details'
   	validates_length_of :first_name, minimum: 2, maximum: 255
   	validates_length_of :last_name, minimum: 2, maximum: 255
-
-  	validates :first_name, format: { with: /[a-zA-Z]/, message: 'Should contain only Alphabets'}
-  	validates :last_name, format: { with: /[a-zA-Z]/, message: 'Should contain only Alphabets'}
-  	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Not a valid Email Address' } 
-  	validates_numericality_of :age, allow_blank: true, greater_than_or_equal_to: 18, less_than_or_equal_to: 100, message: 'must be between 18 & 100'
+  	validates :first_name, format: { with: ALPHABET_FORMAT, message: 'Should contain only Alphabets'}
+    validates :last_name, format: { with: ALPHABET_FORMAT, message: 'Should contain only Alphabets'}
+    validates_numericality_of :age, allow_blank: true, less_than_or_equal_to: 100, message: 'must be between 18 & 100'
 
 end
